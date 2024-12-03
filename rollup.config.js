27f90e6bba269ebc43a2a80881c0745d91543275
clumsy-sy <1725798537@qq.com>
2024-12-03 12:14:19 +0800

    import wgsl as String  directly

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,20 @@ import typescript from '@rollup/plugin-typescript';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 
+function wgsl2String() {
+  return {
+    name: 'wgsl-plugin',
+    transform(code, id) {
+      if (id.endsWith('.wgsl')) {
+        return {
+          code: `export default \`${code}\`;`,
+          map: { mappings: '' },
+        };
+      }
+    },
+  };
+}
+
 export default {
   input: 'src/main.ts', // 项目入口文件
   output: {
@@ -10,6 +24,7 @@ export default {
     sourcemap: true, // 生成 source map
   },
   plugins: [
+    wgsl2String(), // 处理 wgsl 文件
     typescript(), // TypeScript 转换插件
     serve({
       open: true, // 启动服务器时自动打开浏览器
